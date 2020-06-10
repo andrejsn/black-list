0ef173dc3bc5bc8a713f50ffbde3b08ba92fb723
andrejsn <andrejs@naumovs.de>
2020-06-10 14:25:12 +0200

    sorry, was false interceptor

--- src/app/shared/helpers/csrf/interceptor.ts
+++ src/app/shared/helpers/csrf/interceptor.ts
@@ -1,14 +1,35 @@
 import { Injectable } from "@angular/core";
-import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent } from "@angular/common/http";
+import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent, HttpXsrfTokenExtractor } from "@angular/common/http";
 import { Observable } from "rxjs";
+
+// see: https://stackoverflow.com/questions/61063479/csrf-token-mismatch-laravel-sanctum-and-angular-http
 
 @Injectable()
 export class Interceptor implements HttpInterceptor {
+  headerName = 'XSRF-TOKEN';
+
+  constructor(private tokenService: HttpXsrfTokenExtractor) {}
+
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-    const customReq = request.clone({
+    if (request.method === 'GET' || request.method === 'HEAD'){
 
-    });
-    return next.handle(customReq);
+      return next.handle(request);
+    }
+
+    const token = this.tokenService.getToken();
+
+    // Be careful not to overwrite an existing header of the same name.
+    if (token !== null && !request.headers.has(this.headerName))
+    {
+      request = request.clone({headers: request.headers.set(this.headerName, token)});
+    }
+    return next.handle(request);
+
+
+    // const customReq = request.clone({
+
+    // });
+    // return next.handle(customReq);
   }
 }
