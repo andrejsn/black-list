c9050e6d37aba02e58cfff9847db4de39681d704
andrejsn <andrejs@naumovs.de>
2020-06-12 16:46:53 +0200

    added public method

--- src/app/shared/services/authentication/authentication-service.service.ts
+++ src/app/shared/services/authentication/authentication-service.service.ts
@@ -11,19 +11,16 @@ export class AuthenticationService {
 
   constructor(private http: HttpClient) { }
 
-  login(email:string, password:string) {
+  login(email: string, password: string) {
 
-this.http.post<any>(`${environment.apiUrl}/auth/login`,
+    this.http.post<any>(`${environment.apiUrl}/auth/login`,
       {
         'email': email,
         'password': password,
-
       }
     ).pipe(first())
       .subscribe(
-        data => {
-          console.log(data);
-        },
+        res => this.setSession(res),
         error => {
           console.log('error = ' + error);
         }
@@ -31,6 +28,37 @@ this.http.post<any>(`${environment.apiUrl}/auth/login`,
 
   }
 
+  /*
+  * set in session local storage
+  */
+  private setSession(authResult: { access_token: string; expires_in: moment.DurationInputArg1; }) {
+    localStorage.setItem('access_token', authResult.access_token);
+    const expiresAt = moment().add(authResult.expires_in, 'second');
 
+    localStorage.setItem('expires_in', JSON.stringify(expiresAt.valueOf()));
+  }
+
+
+  /*
+  * remove stored user session items
+  */
+  logout() {
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('expires_in');
+  }
+
+  isLoggedIn() {
+    return moment().isBefore(this.getExpiration());
+  }
+
+  isLoggedOut() {
+    return !this.isLoggedIn();
+  }
+
+  private getExpiration() {
+    const expiration = localStorage.getItem("expires_in");
+    const expiresAt = JSON.parse(expiration);
+    return moment(expiresAt);
+  }
 
 }
