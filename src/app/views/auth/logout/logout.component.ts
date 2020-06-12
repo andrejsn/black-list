99548f96b4001ced0a7cbaef7920f821d579263d
andrejsn <andrejs@naumovs.de>
2020-06-12 22:36:41 +0200

    added logout

--- src/app/views/auth/logout/logout.component.ts
+++ src/app/views/auth/logout/logout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../../../shared/helpers/authentication/authentication.service';
 
 @Component({
   selector: 'app-logout',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService) { }
 
   ngOnInit(): void {
+    this.authenticationService.logout();
   }
 
 }
