b5e5b772df8135c06da4ce23af32c364c16a72f6
andrejsn <andrejs@naumovs.de>
2020-06-14 22:29:55 +0200

    i18n breadcrumb

--- src/app/containers/default-layout/default-layout.component.ts
+++ src/app/containers/default-layout/default-layout.component.ts
@@ -16,6 +16,8 @@ export class DefaultLayoutComponent implements AfterViewInit {
   debtors: Element;
   add_debtor: Element;
   calendar: Element;
+  // breadcrumb menu
+  breadcrumb: Element;
 
   // ATTENTION : this values sync to navigation list & en.json|ru.json|lv.json
   reports_re: RegExp = /Reports|Отчёты|Atskaites/g;
@@ -25,6 +27,7 @@ export class DefaultLayoutComponent implements AfterViewInit {
 
   constructor(public translate: TranslateService) { }
   ngAfterViewInit(): void {
+    this.breadcrumb = document.getElementsByClassName('breadcrumb').item(0);
     const elements = document.getElementsByTagName('app-sidebar-nav-link-content');
 
     this.reports = elements.item(0);
@@ -40,12 +43,23 @@ export class DefaultLayoutComponent implements AfterViewInit {
    FIXME use i18n from cui, if exist?
    */
   translateTo(language: string): void {
-
     this.translate.use(language);
+    // translate menu
     this.translate.get('app-sidebar-nav-link.reports').subscribe((res: string) => { this.reports.innerHTML = this.reports.innerHTML.replace(this.reports_re, res); })
     this.translate.get('app-sidebar-nav-link.debtors').subscribe((res: string) => { this.debtors.innerHTML = this.debtors.innerHTML.replace(this.debtors_re, res); })
     this.translate.get('app-sidebar-nav-link.add_debtor').subscribe((res: string) => { this.add_debtor.innerHTML = this.add_debtor.innerHTML.replace(this.add_debtor_re, res); })
     this.translate.get('app-sidebar-nav-link.calendar').subscribe((res: string) => { this.calendar.innerHTML = this.calendar.innerHTML.replace(this.calendar_re, res); })
+
+    // translate breadcrumb
+    if (this.reports_re.test(this.breadcrumb.innerHTML)) {
+      this.translate.get('app-sidebar-nav-link.reports').subscribe((res: string) => { this.breadcrumb.innerHTML = this.breadcrumb.innerHTML.replace(this.reports_re, res); })
+    } else if (this.debtors_re.test(this.breadcrumb.innerHTML)) {
+      this.translate.get('app-sidebar-nav-link.debtors').subscribe((res: string) => { this.breadcrumb.innerHTML = this.breadcrumb.innerHTML.replace(this.debtors_re, res); })
+    } else if (this.add_debtor_re.test(this.breadcrumb.innerHTML)) {
+      this.translate.get('app-sidebar-nav-link.add_debtor').subscribe((res: string) => { this.breadcrumb.innerHTML = this.breadcrumb.innerHTML.replace(this.add_debtor_re, res); })
+    } else if (this.calendar_re.test(this.breadcrumb.innerHTML)) {
+      this.translate.get('app-sidebar-nav-link.calendar').subscribe((res: string) => { this.breadcrumb.innerHTML = this.breadcrumb.innerHTML.replace(this.calendar_re, res); })
+    }
   }
 
   toggleMinimize(e: boolean) {
