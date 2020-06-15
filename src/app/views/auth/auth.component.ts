a47f31c84cc5aace4f55c3ac0425354e548676dc
andrejsn <andrejs@naumovs.de>
2020-06-16 01:21:18 +0200

    fixed build production - set empty template(?)

--- src/app/views/auth/auth.component.ts
+++ src/app/views/auth/auth.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'body',
-  template: '<router-outlet></router-outlet>'
+  // template:  '<router-outlet></router-outlet>'
+  template: ''
 })
 export class AuthComponent implements OnInit {
 
