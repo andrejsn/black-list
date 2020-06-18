d6756022d82a167e4714fdeea65de28c831ce04e
andrejsn <andrejs@naumovs.de>
2020-06-18 13:29:40 +0200

    added: company name & status select

--- src/app/views/add/debtor/debtor.component.ts
+++ src/app/views/add/debtor/debtor.component.ts
@@ -8,13 +8,26 @@ import { Status } from '@app/models';
 })
 export class DebtorComponent implements OnInit {
 
+  company: string;
 
+  all_statuses  = Status;
 
   constructor() { }
 
-
-
   ngOnInit(): void {
+
+  }
+
+
+  companyNameChanged(companyName:string) {
+    console.log(companyName);
+
+  }
+
+  statusChanged(status) {
+    console.log(status);
+
+
   }
 
 }
