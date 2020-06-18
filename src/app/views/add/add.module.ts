7dcbafc81c2f9505bfee248150959c045640167f
andrejsn <andrejs@naumovs.de>
2020-06-18 16:05:04 +0200

    load TranslateModule for child

--- src/app/views/add/add.module.ts
+++ src/app/views/add/add.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { AddRoutingModule } from './add-routing.module';
 import { NgxEditorModule } from 'ngx-editor';
 import { DebtorComponent } from './debtor/debtor.component';
+import { HttpLoaderFactory } from '@app/app.module';
+import { HttpClient } from '@angular/common/http';
 
 
 @NgModule({
@@ -14,6 +17,8 @@ import { DebtorComponent } from './debtor/debtor.component';
     FormsModule,
     //NgxEditorModule,
     AddRoutingModule,
+    TranslateModule
+    //TranslateModule.forRoot({ defaultLanguage: 'en', loader: { provide: TranslateLoader, useFactory: HttpLoaderFactory, deps: [HttpClient] } }),
   ]
 })
 export class AddModule { }
