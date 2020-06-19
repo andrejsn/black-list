8f62f05a2a5ab66a059b1c941114a53010abe290
andrejsn <andrejs@naumovs.de>
2020-06-19 19:42:11 +0200

    added from group

--- src/app/views/add/debtor/debtor.component.ts
+++ src/app/views/add/debtor/debtor.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { statuses } from '@app/models';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-debtor',
@@ -8,26 +9,53 @@ import { statuses } from '@app/models';
 })
 export class DebtorComponent implements OnInit {
 
-  company: string;
+  //company: string;
+  all_statuses: string[] = statuses;
 
-  all_statuses:string[]  = statuses;
+  addDebtorForm: FormGroup = new FormGroup(
+    {
+      company: new FormControl(),
+      reg_number: new FormControl(),
+      debt: new FormControl(),
 
-  constructor() { }
+      legal_address: new FormControl(),
+      city: new FormControl(),
+      postal_code: new FormControl(),
+      country: new FormControl(),
+      phone: new FormControl(),
+      fax: new FormControl(),
+      email: new FormControl(),
+      homepage: new FormControl(),
+      bank_name: new FormControl(),
+      bank_account_number: new FormControl(),
+      status: new FormControl(),
+      note: new FormControl(),
+    }
+  );
+
+  constructor(private formBuilder: FormBuilder,) { }
 
   ngOnInit(): void {
-
+    this.addDebtorForm = this.formBuilder.group(
+      {
+        company: ['', [Validators.required]], // TODO min oder max length?
+        reg_number: ['', [Validators.required]], // TODO min oder max length?
+        debt: ['', [Validators.required]], // TODO min oder max length? number?
+      }
+    );
   }
 
 
-  companyNameChanged(companyName:string) {
+  companyNameChanged(companyName: string) {
     console.log(companyName);
 
   }
 
   statusChanged(status) {
     console.log(status);
-
-
   }
-
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.addDebtorForm.controls;
+  }
 }
