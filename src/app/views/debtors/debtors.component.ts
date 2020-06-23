6e9ff1cbfc932194beeae556c28aa729a0c9ae5c
andrejsn <andrejs@naumovs.de>
2020-06-23 19:16:24 +0200

    add first logic to debtors components

--- src/app/views/debtors/debtors.component.ts
+++ src/app/views/debtors/debtors.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '@environments/environment';
+import { first } from 'rxjs/operators';
+import { Debtor } from '@app/models';
 
 @Component({
   selector: 'app-debtors',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DebtorsComponent implements OnInit {
 
-  constructor() { }
+  debtors: Debtor[];
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<any>(`${environment.apiUrl}/get/debtors`,
+{}
+    ).pipe(first())
+      .subscribe(
+        data => {
+          this.debtors = data;
+          console.log(this.debtors);
+
+        },
+        error => {
+          console.log(error);
+
+        }
+      );
   }
 
 }
