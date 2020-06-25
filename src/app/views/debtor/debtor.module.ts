81e3c59f1a1e1373b6e2dc549686cc60fdb22381
andrejsn <andrejs@naumovs.de>
2020-06-25 14:21:16 +0200

    added components to details

--- src/app/views/debtor/debtor.module.ts
+++ src/app/views/debtor/debtor.module.ts
@@ -4,10 +4,15 @@ import { CommonModule } from '@angular/common';
 import { DebtorRoutingModule } from './debtor-routing.module';
 import { DebtorComponent } from './debtor.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { ContractsComponent } from './details/contracts/contracts.component';
+import { InvoicesComponent } from './details/invoices/invoices.component';
+import { RepresentativesComponent } from './details/representatives/representatives.component';
+import { GuarantorsComponent } from './details/guarantors/guarantors.component';
+import { DocumentsComponent } from './details/documents/documents.component';
 
 
 @NgModule({
-  declarations: [DebtorComponent],
+  declarations: [DebtorComponent, ContractsComponent, InvoicesComponent, RepresentativesComponent, GuarantorsComponent, DocumentsComponent],
   imports: [
     CommonModule,
     DebtorRoutingModule,
