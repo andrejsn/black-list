cc8b5397d16fe7970bae83f2cfe65d7ad754a94b
andrejsn <andrejs@naumovs.de>
2020-06-25 23:41:01 +0200

    added table header to contracts

--- src/app/views/debtor/details/contracts/contracts.component.ts
+++ src/app/views/debtor/details/contracts/contracts.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '@environments/environment';
+import { Router } from '@angular/router';
+
 import { first } from 'rxjs/operators';
+
+import { environment } from '@environments/environment';
 import { DebtorCachedService } from '@shared/services';
+import { Contract } from '@app/models/contract';
 
 @Component({
   selector: 'app-contracts',
@@ -11,20 +15,30 @@ import { DebtorCachedService } from '@shared/services';
 })
 export class ContractsComponent implements OnInit {
 
+  contracts: Contract[];
+
   constructor(
     private debtorCachedService: DebtorCachedService,
+    private router: Router,
     private http: HttpClient,
     ) { }
 
   ngOnInit(): void {
+    if(!this.debtorCachedService.debtor) {
+      // no debtor cached
+      this.router.navigate(['/debtors']);
+
+      return;
+    }
+
     // get data
     this.http.get<any>(`${environment.apiUrl}/get/debtor/` + this.debtorCachedService.debtor.id + `/contracts`,
       {}
     ).pipe(first())
       .subscribe(
         data => {
-
-          console.log(data);
+          this.contracts = data;
+          console.log(this.contracts);
 
         },
         error => {
