539877b18a4c79c6580dac6e4eeef6c7c0fd3900
andrejsn <andrejs@naumovs.de>
2020-06-25 22:25:52 +0200

    added http subscribe to get contracts route

--- src/app/views/debtor/details/contracts/contracts.component.ts
+++ src/app/views/debtor/details/contracts/contracts.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '@environments/environment';
+import { first } from 'rxjs/operators';
+import { DebtorCachedService } from '@shared/services';
 
 @Component({
   selector: 'app-contracts',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContractsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private debtorCachedService: DebtorCachedService,
+    private http: HttpClient,
+    ) { }
 
   ngOnInit(): void {
+    // get data
+    this.http.get<any>(`${environment.apiUrl}/get/debtor/` + this.debtorCachedService.debtor.id + `/contracts`,
+      {}
+    ).pipe(first())
+      .subscribe(
+        data => {
+
+          console.log(data);
+
+        },
+        error => {
+          console.log(error);
+
+        }
+      );
   }
 
 }
