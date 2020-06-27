974fd609a7f0bdd234d3caaee823f8cae7bdff94
andrejsn <andrejs@naumovs.de>
2020-06-27 22:09:37 +0200

    added template component

--- src/app/views/debtor/debtor.module.ts
+++ src/app/views/debtor/debtor.module.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 import { DebtorRoutingModule } from './debtor-routing.module';
 import { DetailsComponent } from './details/details.component';
 import { ContractsComponent, InvoicesComponent, RepresentativesComponent, GuarantorsComponent, DocumentsComponent } from '.';
+import { TemplateComponent } from './template/template.component';
 
 
 
 @NgModule({
-  declarations: [DetailsComponent, ContractsComponent, InvoicesComponent, RepresentativesComponent, GuarantorsComponent, DocumentsComponent],
+  declarations: [DetailsComponent, ContractsComponent, InvoicesComponent, RepresentativesComponent, GuarantorsComponent, DocumentsComponent, TemplateComponent],
   imports: [
     CommonModule,
     DebtorRoutingModule
