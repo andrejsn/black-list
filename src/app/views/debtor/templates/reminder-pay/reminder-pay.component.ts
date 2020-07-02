4b45198b844afef0ddbe03985d02729b5a8049d6
andrejsn <andrejs@naumovs.de>
2020-07-02 11:27:05 +0200

    added error handle

--- src/app/views/debtor/templates/reminder-pay/reminder-pay.component.ts
+++ src/app/views/debtor/templates/reminder-pay/reminder-pay.component.ts
@@ -1,7 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, Input } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { first } from 'rxjs/operators';
+import { TranslateService } from '@ngx-translate/core';
+import { SnotifyService } from 'ng-snotify';
+
 
 import { Contract } from '@app/models';
+import { environment } from '@environments/environment';
 
 @Component({
   selector: 'app-reminder-pay',
@@ -15,19 +22,59 @@ export class ReminderPayComponent implements OnInit {
   submitted: boolean = false;
   loading: boolean = false;
 
-
-
-  form = new FormGroup({
+  reminderPayForm = new FormGroup({
+    place: new FormControl(),
+    number: new FormControl(),
+    days: new FormControl(),
     date: new FormControl(new Date()),
   });
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+    private translate: TranslateService,
+    private http: HttpClient,
+    private snotifyService: SnotifyService) { }
 
   ngOnInit(): void {
-
     console.log(this.contract);
+
+    this.reminderPayForm = this.formBuilder.group(
+      {
+        place: ['', [Validators.required]],
+        number: ['', Validators.required],
+        days: ['', [Validators.required]],
+        date: ['', [Validators.required]]
+      }
+    );
 
 
   }
 
+  /**
+   *
+   */
+  onSubmit() {
+console.log(this.reminderPayForm.controls['date'].value);
+
+
+
+
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.reminderPayForm.invalid) {
+      this.translate.get('toast.error.template.form').subscribe((error: string) => { this.snotifyService.error(error) });
+
+      return;
+    }
+
+    this.loading = true;
+  }
+
+
+
+
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.reminderPayForm.controls;
+  }
 }
