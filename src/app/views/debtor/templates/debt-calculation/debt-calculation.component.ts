39c740f0df8dbdd18d125418f27c8c276199d2c8
andrejsn <andrejs@naumovs.de>
2020-07-04 18:16:12 +0200

    added debt calculation form

--- src/app/views/debtor/templates/debt-calculation/debt-calculation.component.ts
+++ src/app/views/debtor/templates/debt-calculation/debt-calculation.component.ts
@@ -1,4 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { first } from 'rxjs/operators';
+import { TranslateService } from '@ngx-translate/core';
+import { SnotifyService } from 'ng-snotify';
+
+
+import { Contract } from '@app/models';
+import { environment } from '@environments/environment';
+
+interface ContractCalcPay extends Contract {
+  calcDate: Date, saveDoc: boolean
+}
 
 @Component({
   selector: 'app-debt-calculation',
@@ -7,9 +21,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DebtCalculationComponent implements OnInit {
 
-  constructor() { }
+  @Input() contract: ContractCalcPay;
+
+  submitted: boolean = false;
+  loading: boolean = false;
+
+  calcPayForm = new FormGroup({
+    calcDate: new FormControl(new Date()),
+    saveDoc: new FormControl()
+  });
+
+  constructor(private formBuilder: FormBuilder,
+    private translate: TranslateService,
+    private http: HttpClient,
+    private snotifyService: SnotifyService) { }
 
   ngOnInit(): void {
+    this.calcPayForm = this.formBuilder.group(
+      {
+        calcDate: ['', [Validators.required]],
+        saveDoc: ['', '']
+      }
+    );
+    this.calcPayForm.patchValue({ saveDoc: true });
+  }
+  /**
+   * submit form
+   */
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.calcPayForm.invalid) {
+      this.translate.get('toast.error.template.form').subscribe((error: string) => { this.snotifyService.error(error) });
+
+      return;
+    }
+
+    this.loading = true;
+
+    this.contract.calcDate = this.calcPayForm.controls['calcDate'].value;
+    this.contract.saveDoc = this.calcPayForm.controls['saveDoc'].value;
+
+    this.http.post<any>(`${environment.apiUrl}/pdf/contract/calc`,
+      {
+        'contract': this.contract
+      }, { responseType: 'blob' as 'json' }
+    ).pipe(first())
+      .subscribe(
+        data => {
+           console.log(data);
+
+
+         // window.open(window.URL.createObjectURL(data));
+        },
+        error => {
+
+
+          console.log(error);
+
+
+          this.loading = false;
+          this.submitted = false;
+          this.translate.get('toast.error.response').subscribe((error: string) => { this.snotifyService.error(error) });
+        }
+      );
   }
 
+
+
+
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.calcPayForm.controls;
+  }
 }
