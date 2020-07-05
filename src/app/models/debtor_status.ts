e33a82a309136b49859113aaa0b9534db681b6c0
andrejsn <andrejs@naumovs.de>
2020-07-05 09:00:55 +0200

    fixed enum - remove strings

--- src/app/models/debtor_status.ts
+++ src/app/models/debtor_status.ts
@@ -1,10 +1,10 @@
 // TODO: change to enum?
 export enum DebtorStatus {
-  'awaiting', // badge-secondary
-  'in_work', // badge-primary
-  'debt_collected', // badge-success
-  'to_sue', // badge-light
-  'in_court', // badge-info
-  'hopeless', // badge-warning
-  'insolvency', // badge-danger
+  awaiting, // badge-secondary
+  in_work, // badge-primary
+  debt_collected, // badge-success
+  to_sue, // badge-light
+  in_court, // badge-info
+  hopeless, // badge-warning
+  insolvency, // badge-danger
 }
