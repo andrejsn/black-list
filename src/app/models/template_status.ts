ce48e38d0e1a767a4d4fd88907e35f145c44fea8
andrejsn <andrejs@naumovs.de>
2020-07-05 09:07:19 +0200

    fixed: remove strings

--- src/app/models/template_status.ts
+++ src/app/models/template_status.ts
@@ -1,3 +1,3 @@
 export enum TemplateStatus {
-  'reminder_pay', 'warning_pay', 'debt_calculation', 'warning_pre_trial', 'claim_to_court', 'creditor_to_administrator'
+  reminder_pay, warning_pay, debt_calculation, warning_pre_trial, claim_to_court, creditor_to_administrator
 }
