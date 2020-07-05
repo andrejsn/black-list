4289c3616d35d1812cadfbf4e798f9aa43c09884
andrejsn <andrejs@naumovs.de>
2020-07-05 08:56:29 +0200

    added enum and interface

--- src/app/views/debtor/templates/claim-to-court/claim-to-court.component.ts
+++ src/app/views/debtor/templates/claim-to-court/claim-to-court.component.ts
@@ -1,4 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { first } from 'rxjs/operators';
+import { TranslateService } from '@ngx-translate/core';
+import { SnotifyService } from 'ng-snotify';
+
+
+import { Contract } from '@app/models';
+import { environment } from '@environments/environment';
+
+enum Courts {
+  arbitration,
+  state
+}
+interface ContractClaimToCourt extends Contract {
+  place: string, number: string, judgesNumber: number, court: Courts, stateCourtName: string, stateCourtAddress: string, attachments: string[], claimToCourtDate: Date, saveDoc: boolean
+}
 
 @Component({
   selector: 'app-claim-to-court',
@@ -13,3 +31,4 @@ export class ClaimToCourtComponent implements OnInit {
   }
 
 }
+
