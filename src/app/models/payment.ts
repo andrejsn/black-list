72861f1368232b1b6fa64178cc2da251fdc65aee
andrejsn <andrejs@naumovs.de>
2020-07-11 14:19:39 +0200

    added payment model

---
+++ src/app/models/payment.ts
@@ -0,0 +1,13 @@
+import { PaymentStatus } from './payment_status';
+
+export interface Payment {
+  id: number;
+  invoice_id: number;
+  date: Date;
+  sum: number;
+  status: PaymentStatus;
+
+  created_at: Date;
+  deleted_at: Date;
+  updated_at: Date;
+}
---
+++ src/app/models/payment_status.ts
@@ -0,0 +1,3 @@
+export enum PaymentStatus {
+  'debt', 'fine'
+}
