0ebf37cbbce21e7dad4ba94c938d5221e9e02dff
andrejsn <andrejs@naumovs.de>
2020-07-14 23:52:57 +0200

    added payments as input

--- src/app/views/debtor/payments/payments.component.ts
+++ src/app/views/debtor/payments/payments.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Payment } from '@app/models';
+
 
 @Component({
   selector: 'app-payments',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payments.component.css']
 })
 export class PaymentsComponent implements OnInit {
+
+  @Input() payments: Payment;
 
   constructor() { }
 
