d0dfff9d45c0b27f451aafdb821cbdad3b183b4a
andrejsn <andrejs@naumovs.de>
2020-07-16 18:06:34 +0200

    added value to enum

--- src/app/models/debtor_status.ts
+++ src/app/models/debtor_status.ts
@@ -1,10 +1,10 @@
 // TODO: change to enum?
 export enum DebtorStatus {
-  awaiting, // badge-secondary
-  in_work, // badge-primary
-  debt_collected, // badge-success
-  to_sue, // badge-light
-  in_court, // badge-info
-  hopeless, // badge-warning
-  insolvency, // badge-danger
+  awaiting = 0, // badge-secondary
+  in_work = 1, // badge-primary
+  debt_collected = 2, // badge-success
+  to_sue = 3, // badge-light
+  in_court = 4, // badge-info
+  hopeless = 5, // badge-warning
+  insolvency = 6, // badge-danger
 }
