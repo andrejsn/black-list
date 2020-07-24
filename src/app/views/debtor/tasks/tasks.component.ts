5137abdce4bfd45817d7551558794e1612760b97
andrejsn <andrejs@naumovs.de>
2020-07-24 19:40:11 +0200

    added read data from backend

--- src/app/views/debtor/tasks/tasks.component.ts
+++ src/app/views/debtor/tasks/tasks.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Debtor } from '@app/models';
+import { DebtorCachedService } from '@shared/services';
+import { Router } from '@angular/router';
+import { Calendar } from '@app/models/calendar';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '@environments/environment';
+import { first } from 'rxjs/operators';
 
+interface CalendarTableElement extends Calendar{
+  // empty extends
+}
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
@@ -7,9 +17,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasksComponent implements OnInit {
 
-  constructor() { }
+  debtor: Debtor;
+  calendarList: CalendarTableElement[];
+
+  constructor(
+    private debtorCachedService: DebtorCachedService,
+    private router: Router,
+    private http: HttpClient,
+    ) { }
 
   ngOnInit(): void {
+    if(!this.debtorCachedService.debtor) {
+      // no debtor cached
+      this.router.navigate(['/debtors']);
+
+      return;
+    }
+
+    this.debtor = this.debtorCachedService.debtor;
+
+    // get data
+    this.http.get<any>(`${environment.apiUrl}/get/debtor/` + this.debtor.id + `/calendars`,
+      {}
+    ).pipe(first())
+      .subscribe(
+        data => {
+          this.calendarList = data;
+
+          console.log(this.calendarList);
+
+
+        },
+        error => {
+          console.log(error);
+        }
+      );
   }
 
 }
