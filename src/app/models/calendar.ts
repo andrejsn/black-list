0dd38d9e9e34768d2dc88bc53093ee8639793585
Andrejs Naumovs <andrejs@naumovs.de>
2020-07-25 16:00:47 +0300

    added created, updated and deleted at

--- src/app/models/calendar.ts
+++ src/app/models/calendar.ts
@@ -5,11 +5,15 @@ export interface Calendar {
   created_by: number;
   debtor_id: number;
 
-  date:Date;
+  date: Date;
   note: string;
 
   remind_date: Date;
   remind_note: string;
   remind_status: CalendarStatus;
   remind_done: boolean;
+
+  created_at: Date;
+  updated_at: Date;
+  deleted_at: Date;
 }
