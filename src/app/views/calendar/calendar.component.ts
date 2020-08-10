963bf97ba5b3b3e903b3f59202f533bfa47b017b
Andrejs Naumovs <andrejs@naumovs.de>
2020-08-10 19:48:11 +0300

    added: year switch

--- src/app/views/calendar/calendar.component.ts
+++ src/app/views/calendar/calendar.component.ts
@@ -25,83 +25,48 @@ export interface CalendarDate {
   styleUrls: ['./calendar.component.scss'],
 })
 export class CalendarComponent implements OnInit {
-  public currentDate: moment.Moment;
-
+  public selectedYear: number;
   public months: Month[] = [];
-  // public namesOfMonths = [
-  //   'January',
-  //   'February',
-  //   'March',
-  //   'April',
-  //   'May',
-  //   'June',
-  //   'July',
-  //   'August',
-  //   'September',
-  //   'October',
-  //   'November',
-  //   'December',
-  // ];
-  // public namesOfDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-  // public namesOfDays = ['Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So'];
-
   public namesOfDays: string[] = [];
-
-  public selectedDate;
-
-  @ViewChild('calendar', { static: true }) calendar;
-
-  @HostListener('document:click', ['$event'])
-  clickOut(event) {
-    if (!this.eRef.nativeElement.contains(event.target)) {
-      // this.show = false;
-      console.log('hello+++');
-    }
-  }
 
   constructor(private eRef: ElementRef) {}
 
   ngOnInit(): void {
     moment.locale('lv');
+
     this.namesOfDays = moment.weekdaysShort();
     this.namesOfDays.push(
       /*set monday as week start*/ this.namesOfDays.shift()
     );
 
-    // moment.updateLocale('en', {
-    //   week: {
-    //     dow: 1, // First day of week is Monday
-    //     // doy: 7, // First week of year must contain 1 January (7 + 1 - 1)
-    //   },
-    // });
+    this.selectedYear = moment().year();
+    this.generateCalendarOfYear();
+  }
 
-    // this.currentDate = moment();
-    // this.selectedDate = moment(this.currentDate).format('DD/MM/YYYY');
-    // this.generateCalendar();
-
-    // create months for current year
+  /**
+   * generate full calendar
+   * @param year -  current year
+   */
+  generateCalendarOfYear() {
+    // clear months
+    this.months = [];
+    // create for 12 months
     for (let i = 0; i < 12; i++) {
-      const firstDay = moment([2020, i, 1]);
+      const firstDay = moment([this.selectedYear, i, 1]);
       const dates = this.fillDates(firstDay);
 
       const weeks = [];
       while (dates.length > 0) {
         weeks.push(dates.splice(0, 7));
       }
-      // console.log(weeks);
+
       this.months.push({ firstDay: firstDay, weeks: weeks });
     }
   }
 
-  private generateCalendar(): void {
-    const dates = this.fillDates(this.currentDate);
-    const weeks = [];
-    while (dates.length > 0) {
-      weeks.push(dates.splice(0, 7));
-    }
-    // this.weeks = weeks;
-  }
-
+  /**
+   * fill dates for month calendar
+   */
   private fillDates(currentMoment: moment.Moment) {
     const firstOfMonth = moment(currentMoment)
       .startOf('month')
@@ -136,47 +101,24 @@ export class CalendarComponent implements OnInit {
   }
 
   public prevYear(): void {
-    console.log('get to preview year');
+    this.selectedYear--;
+    this.generateCalendarOfYear();
   }
 
   public nextYear(): void {
-    console.log('get to next year');
+    this.selectedYear++;
+    this.generateCalendarOfYear();
   }
 
-  public prevMonth(): void {
-    this.currentDate = moment(this.currentDate).subtract(1, 'months');
-    this.generateCalendar();
-  }
-
-  public nextMonth(): void {
-    this.currentDate = moment(this.currentDate).add(1, 'months');
-    this.generateCalendar();
-  }
-
-  public isDisabledMonth(currentDate): boolean {
-    const today = moment();
-    return moment(currentDate).isBefore(today, 'months');
+  public isSelected(date: moment.Moment) {
+    return false;
   }
 
   private isToday(date: moment.Moment): boolean {
     return moment().isSame(moment(date), 'day');
   }
 
-  private isSelected(date: moment.Moment): boolean {
-    return this.selectedDate === moment(date).format('DD/MM/YYYY');
-  }
-
-  public isSelectedMonth(date: moment.Moment): boolean {
-    const today = moment();
-    return (
-      moment(date).isSame(this.currentDate, 'month') &&
-      moment(date).isSameOrBefore(today)
-    );
-  }
-
   public isDayOfThisMonth(month: Month, date: moment.Moment) {
-
-
     return moment(month.firstDay).isSame(date, 'month');
   }
 
