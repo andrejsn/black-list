6efa7ef7b8decfd4479e27307fd7b80a3dad1a6c
Andrejs Naumovs <andrejs@naumovs.de>
2020-08-13 22:58:00 +0300

    created: calendar2

--- src/app/views/calendar/calendar-routing.module.ts
+++ src/app/views/calendar/calendar-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CalendarComponent } from './calendar.component';
+import { CalendarComponent } from './calendar2/calendar/calendar.component';
 
 
 const routes: Routes = [
