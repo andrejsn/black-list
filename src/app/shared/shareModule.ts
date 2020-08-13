77f30e450b858a6fa7bc44dfadd3f4456f3a0be2
Andrejs Naumovs <andrejs@naumovs.de>
2020-08-14 00:35:25 +0300

    updated to angular10 ivy&fixed ModuleWithProviders

--- src/app/shared/shareModule.ts
+++ src/app/shared/shareModule.ts
@@ -3,6 +3,14 @@ import { EnumToArrayPipe } from './helpers';
 // import { appDirective } from './{your-path}';
 // import { appService } from './{your-path}';
 
+// fixed error: https://stackoverflow.com/questions/62755093/angular-error-generic-type-modulewithproviderst-requires-1-type-arguments
+declare module '@angular/core' {
+  interface ModuleWithProviders<T = any> {
+    ngModule: Type<T>;
+    providers?: Provider[];
+  }
+}
+
 @NgModule({
   declarations: [
     EnumToArrayPipe,
@@ -11,7 +19,7 @@ import { EnumToArrayPipe } from './helpers';
   exports: [
     EnumToArrayPipe,
     // appDirective
-  ]
+  ],
 })
 export class SharingModule {
   static forRoot(): ModuleWithProviders {
