10570ac86d2651e000ecb8b7585faf758e24c763
Andrejs Naumovs <andrejs@naumovs.de>
2020-08-28 11:36:53 +0300

    added: debtor details

--- src/app/views/debtor/contracts/contracts.component.ts
+++ src/app/views/debtor/contracts/contracts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 
 import { first } from 'rxjs/operators';
@@ -18,14 +19,14 @@ interface ContractTableElement extends Contract {
   styleUrls: ['./contracts.component.scss'],
 })
 export class ContractsComponent implements OnInit {
-  debtor: Debtor;
+  selectedDebtor: Debtor;
   contractsList: ContractTableElement[];
-  count: number;
 
   constructor(
+    private title: Title,
+    private objectsService: ObjectsService,
     private router: Router,
-    private http: HttpClient,
-    private objectsService: ObjectsService
+    private http: HttpClient
   ) {}
 
   ngOnInit(): void {
@@ -35,20 +36,34 @@ export class ContractsComponent implements OnInit {
 
       return;
     }
+    this.selectedDebtor = this.objectsService.debtor;
 
-    this.debtor = this.objectsService.debtor;
+    // set browser title
+    this.title.setTitle(this.selectedDebtor.company + '- contracts list');
+    // set bread crumb menu
+    this.objectsService.setBreadCrumb([
+      { route: '/', name: 'Home', active: false },
+      { route: '/debtors', name: 'Debtors', active: false },
+      {
+        route: '/debtor',
+        name: 'Debtor: ' + this.selectedDebtor.company,
+        active: false,
+      },
+      { route: '/debtor/contracts', name: 'Contracts', active: true },
+    ]);
 
     // get data
     this.http
       .get<any>(
-        `${environment.apiUrl}/get/debtor/` + this.debtor.id + `/contracts`,
+        `${environment.apiUrl}/get/debtor/` +
+          this.selectedDebtor.id +
+          `/contracts`,
         {}
       )
       .pipe(first())
       .subscribe(
         (data) => {
           this.contractsList = data;
-          this.count = this.contractsList.length;
         },
         (error) => {
           console.log(error);
