eccb6407912756d0975dbb6f3ebcdcc818162200
Andrejs Naumovs <andrejs@naumovs.de>
2020-09-02 21:21:06 +0300

    added. HTML for invoice edit

--- src/app/views/_edit/invoice/invoice.component.ts
+++ src/app/views/_edit/invoice/invoice.component.ts
@@ -1,15 +1,165 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { first } from 'rxjs/operators';
+import { TranslateService } from '@ngx-translate/core';
+import { SnotifyService, Snotify } from 'ng-snotify';
+import * as reject from 'lodash.reject';
+import * as moment from 'moment';
+
+import { Debtor, Contract, Invoice, InvoiceStatus } from '@app/models';
+import { environment } from '@environments/environment';
+import { ObjectsService } from '@shared/services';
 
 @Component({
   selector: 'app-invoice',
   templateUrl: './invoice.component.html',
-  styleUrls: ['./invoice.component.css']
+  styleUrls: ['./invoice.component.css'],
 })
 export class InvoiceComponent implements OnInit {
+  selectedDebtor: Debtor;
+  selectedContract: Contract;
+  selectedInvoice: Invoice;
+  invoiceStatus = InvoiceStatus;
 
-  constructor() { }
+  editInvoiceForm = new FormGroup({
+    number: new FormControl(),
+    invoiceDate: new FormControl(),
+    dateTo: new FormControl(),
+    sum: new FormControl(),
+    pay_in_days: new FormControl(),
+    status: new FormControl(),
+    note: new FormControl(),
+  });
+
+  submitted: boolean = false;
+  loading: boolean = false;
+
+  constructor(
+    private title: Title,
+    private objectsService: ObjectsService,
+    private router: Router,
+    private translate: TranslateService,
+    private http: HttpClient,
+    private formBuilder: FormBuilder,
+    private snotifyService: SnotifyService
+  ) {}
 
   ngOnInit(): void {
+    if (
+      !this.objectsService.debtor &&
+      !this.objectsService.contract &&
+      !this.objectsService.invoice
+    ) {
+      // no debtor&contract&invoice cached
+      this.router.navigate(['/debtors']);
+
+      return;
+    }
+    this.selectedDebtor = this.objectsService.debtor;
+    this.selectedContract = this.objectsService.contract;
+    this.selectedInvoice = this.objectsService.invoice;
+
+    // set browser title
+    this.title.setTitle(this.selectedDebtor.company + '- edit guarantor');
+    // set bread crumb menu
+    this.objectsService.setBreadCrumb([
+      { route: '/', name: 'Home', active: false },
+      { route: '/debtors', name: 'Debtors', active: false },
+      {
+        route: '/debtor',
+        name: 'Debtor: ' + this.selectedDebtor.company,
+        active: false,
+      },
+      {
+        route: '/contract',
+        name: 'Contract: ' + this.selectedContract.number,
+        active: true,
+      },
+      {
+        route: '/edit/invoice',
+        name: 'Edit invoice: ' + this.selectedInvoice.number,
+        active: true,
+      },
+    ]);
+
+    // create validation
+    const regexPattern: RegExp = new RegExp('^[1-9]{1,5}d*$');
+    this.editInvoiceForm = this.formBuilder.group(
+      {
+        number: [this.selectedInvoice.number, [Validators.required]],
+        invoiceDate: [
+          moment(this.selectedInvoice.date).format('YYYY. DD. MMMM'),
+          [Validators.required],
+        ],
+        dateTo: [
+          moment(this.selectedInvoice.date_to).format('YYYY. DD. MMMM'),
+          [Validators.required],
+        ],
+        sum: [
+          this.selectedInvoice.sum,
+          [Validators.required, Validators.min(0.01)],
+        ],
+        pay_in_days: [
+          // this.selectedInvoice.???,
+          12345,
+          [Validators.required, Validators.pattern(regexPattern)],
+        ],
+        status: [this.selectedInvoice.status, [Validators.required]],
+        note: [this.selectedInvoice.note, []],
+      },
+      { validator: this.dateTo_less_invoiceDate }
+    );
   }
 
+  /**
+   * Validator: DateTo after or equals InvoiceDate
+   */
+  private dateTo_less_invoiceDate(formGroup: FormGroup): any {
+    let invoiceDateTimestamp, invoiceDateToTimestamp;
+    // tslint:disable-next-line: forin
+    for (const controlName in formGroup.controls) {
+      if (controlName.indexOf('invoiceDate') !== -1) {
+        invoiceDateTimestamp = Date.parse(
+          formGroup.controls[controlName].value
+        );
+      }
+      if (controlName.indexOf('dateTo') !== -1) {
+        invoiceDateToTimestamp = Date.parse(
+          formGroup.controls[controlName].value
+        );
+      }
+    }
+    return invoiceDateToTimestamp < invoiceDateTimestamp
+      ? { dateToLessInvoiceDate: true }
+      : null;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.editInvoiceForm.invalid) {
+      this.translate
+        .get('toast.error.template.form')
+        .subscribe((error: string) => {
+          this.snotifyService.error(error);
+        });
+
+      return;
+    }
+  }
+
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.editInvoiceForm.controls;
+  }
 }
