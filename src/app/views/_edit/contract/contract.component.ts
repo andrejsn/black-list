12470339276d066965ab375bbe735b40713a711e
Andrejs Naumovs <andrejs@naumovs.de>
2020-09-09 00:08:58 +0300

    added: contract edit first form

--- src/app/views/_edit/contract/contract.component.ts
+++ src/app/views/_edit/contract/contract.component.ts
@@ -1,15 +1,82 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { first } from 'rxjs/operators';
+import { TranslateService } from '@ngx-translate/core';
+import { SnotifyService, Snotify } from 'ng-snotify';
+import * as reject from 'lodash.reject';
+import * as moment from 'moment';
+
+import { Debtor, Contract, Invoice, InvoiceStatus } from '@app/models';
+import { environment } from '@environments/environment';
+import { ObjectsService } from '@shared/services';
+import { timezoneOffset } from '@shared/helpers';
 
 @Component({
   selector: 'app-contract',
   templateUrl: './contract.component.html',
-  styleUrls: ['./contract.component.css']
+  styleUrls: ['./contract.component.css'],
 })
 export class ContractComponent implements OnInit {
+  selectedDebtor: Debtor;
+  selectedContract: Contract;
 
-  constructor() { }
+  editContractForm = new FormGroup({});
+
+  submitted: boolean = false;
+  loading: boolean = false;
+
+  constructor(
+    private title: Title,
+    private objectsService: ObjectsService,
+    private router: Router,
+    private translate: TranslateService,
+    private http: HttpClient,
+    private formBuilder: FormBuilder,
+    private snotifyService: SnotifyService
+  ) {}
 
   ngOnInit(): void {
+    if (!this.objectsService.debtor && !this.objectsService.contract) {
+      // no debtor&contract cached
+      this.router.navigate(['/debtors']);
+
+      return;
+    }
+    this.selectedDebtor = this.objectsService.debtor;
+    this.selectedContract = this.objectsService.contract;
+
+    // set browser title
+    this.title.setTitle(this.selectedDebtor.company + '- edit contract');
+    // set bread crumb menu
+    this.objectsService.setBreadCrumb([
+      { route: '/', name: 'Home', active: false },
+      { route: '/debtors', name: 'Debtors', active: false },
+      {
+        route: '/debtor',
+        name: 'Debtor: ' + this.selectedDebtor.company,
+        active: false,
+      },
+      {
+        route: '/edit/contract',
+        name: 'Edit contract: ' + this.selectedContract.number,
+        active: true,
+      },
+    ]);
   }
 
+  onSubmit() {}
+
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.editContractForm.controls;
+  }
 }
