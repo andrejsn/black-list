368480aa348e91a0c0f7d31a4216998803c44a27
Andrejs Naumovs <andrejs@naumovs.de>
2020-09-09 01:19:56 +0300

    refactoring: removed unused fields

--- src/app/models/guarantor.ts
+++ src/app/models/guarantor.ts
@@ -1,14 +1,10 @@
 export interface Guarantor {
-  id: number
-  created_by: number;
+  id?: number;
+
   contract_id: number;
   name: string;
   code: string;
   phone: string;
   address: string;
   email: string;
-
-  created_at: Date;
-  updated_at: Date;
-  deleted_at: Date;
 }
