6176a5174e032a008c7b92b34f811f2e3b298673
Andrejs Naumovs <email@adventa.lv>
2020-09-17 13:48:55 +0200

    added: password changed message

--- src/app/views/auth/reset/reset.component.ts
+++ src/app/views/auth/reset/reset.component.ts
@@ -1,23 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import {
   FormGroup,
   FormControl,
   FormBuilder,
   Validators,
-} from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
-import { Router, ActivatedRoute } from '@angular/router';
+} from "@angular/forms";
+import { HttpClient } from "@angular/common/http";
+import { Router, ActivatedRoute } from "@angular/router";
 
-import { TranslateService } from '@ngx-translate/core';
-import { first } from 'rxjs/operators';
-import { SnotifyService } from 'ng-snotify';
+import { TranslateService } from "@ngx-translate/core";
+import { first } from "rxjs/operators";
+import { SnotifyService } from "ng-snotify";
 
-import { environment } from '@environments/environment';
+import { environment } from "@environments/environment";
+import { inOutAnimation } from '@shared/helpers';
 
 @Component({
-  selector: 'app-reset',
-  templateUrl: './reset.component.html',
-  styleUrls: ['./reset.component.scss'],
+  selector: "app-reset",
+  templateUrl: "./reset.component.html",
+  styleUrls: ["./reset.component.scss"],
+  animations: [inOutAnimation()],
 })
 export class ResetComponent implements OnInit {
   token: string;
@@ -25,6 +27,7 @@ export class ResetComponent implements OnInit {
 
   submitted: boolean;
   loading: boolean;
+  isPasswordChanged: boolean;
 
   resetForm: FormGroup = new FormGroup({
     email: new FormControl(),
@@ -43,27 +46,28 @@ export class ResetComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.token = this.route.snapshot.paramMap.get('token');
-    this.key = this.route.snapshot.queryParamMap.get('key');
+    this.isPasswordChanged = false;
+    this.token = this.route.snapshot.paramMap.get("token");
+    this.key = this.route.snapshot.queryParamMap.get("key");
 
     // clear url string
-    this.router.navigate(['/auth/reset/from?email']);
+    this.router.navigate(["/auth/reset/from?email"]);
 
     // TODO: set title?
 
     // set Validators
     this.resetForm = this.formBuilder.group(
       {
-        email: ['', [Validators.required, Validators.email]],
+        email: ["", [Validators.required, Validators.email]],
         password: [
-          '',
+          "",
           [
             Validators.required,
             Validators.minLength(8),
             Validators.maxLength(32),
           ],
         ],
-        password_confirmation: [''],
+        password_confirmation: [""],
       },
       { validator: this.checkPasswords }
     );
@@ -75,8 +79,8 @@ export class ResetComponent implements OnInit {
    */
   private checkPasswords(formGroup: FormGroup) {
     //
-    const pass = formGroup.get('password').value;
-    const confirmPass = formGroup.get('password_confirmation').value;
+    const pass = formGroup.get("password").value;
+    const confirmPass = formGroup.get("password_confirmation").value;
 
     return pass === confirmPass ? null : { notsame: true };
   }
@@ -86,7 +90,7 @@ export class ResetComponent implements OnInit {
     // stop here if form is invalid
     if (this.resetForm.invalid) {
       this.translate
-        .get('toast.error.reset_form')
+        .get("toast.error.reset_form")
         .subscribe((error: string) => {
           this.snotifyService.error(error);
         });
@@ -98,20 +102,20 @@ export class ResetComponent implements OnInit {
 
     this.http
       .post<any>(`${environment.apiUrl}/auth/reset`, {
-        email: this.f['email'].value,
-        password: this.f['password'].value,
-        password_confirmation: this.f['password_confirmation'].value,
+        email: this.f["email"].value,
+        password: this.f["password"].value,
+        password_confirmation: this.f["password_confirmation"].value,
       })
       .pipe(first())
       .subscribe(
         (data) => {
-          console.log(data);
+          this.isPasswordChanged = data && data.success;
         },
         (error) => {
           this.loading = false;
           // TODO: Http failure response error?
           this.translate
-            .get('toast.password.reset.error')
+            .get("toast.password.reset.error")
             .subscribe((err: string) => {
               this.snotifyService.error(error);
             });
