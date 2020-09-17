9b421205e4dda188cbb5726cfa4361eb5f6284eb
Andrejs Naumovs <email@adventa.lv>
2020-09-17 14:23:19 +0200

    added: documents

---
+++ src/app/models/document.ts
@@ -0,0 +1,7 @@
+export interface Document {
+    id?: number;
+
+    name: string;
+    md5?:string;
+    path?:string;
+}
--- src/app/views/debtor/documents/documents.component.ts
+++ src/app/views/debtor/documents/documents.component.ts
@@ -1,7 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, Input } from '@angular/core';
-import { Contract } from '@app/models';
-import { inOutAnimation } from '@shared/helpers';
+import { Router } from '@angular/router';
 
+import { TranslateService } from '@ngx-translate/core';
+import { first } from 'rxjs/operators';
+import { SnotifyService, Snotify } from 'ng-snotify';
+import * as reject from 'lodash.reject';
+
+import { ContractTableElement, Document } from '@app/models';
+import { environment } from '@environments/environment';
+import { inOutAnimation } from '@shared/helpers';
+import { ObjectsService } from '@shared/services';
+
+interface DocumentTableElement extends Document {
+  toDelete: boolean;
+  visible: boolean;
+}
 @Component({
   selector: 'app-documents',
   templateUrl: './documents.component.html',
@@ -9,14 +23,43 @@ import { inOutAnimation } from '@shared/helpers';
   animations: [inOutAnimation()],
 })
 export class DocumentsComponent implements OnInit {
+  @Input() contract: ContractTableElement;
+  documentsList: DocumentTableElement[] = [];
+  visibleList: boolean;
 
-  @Input() contract: Contract;
-  visible: boolean;
+  loading: boolean;
 
-  constructor() { }
+  constructor(
+    private objectsService: ObjectsService,
+    private router: Router,
+    private http: HttpClient,
+    private translate: TranslateService,
+    private snotifyService: SnotifyService
+  ) {}
 
   ngOnInit(): void {
-    // console.log(this.contract.number);
+    // TODO: schould list visible
+    // if -> yes reset another -> see guarantors coponent
+
+    // get Data 
+    // get data
+    this.http
+      .get<any>(
+        `${environment.apiUrl}/get/contract/` +
+          this.contract.id +
+          `/documents`,
+        {}
+      )
+      .pipe(first())
+      .subscribe(
+        (data) => {
+          const tmp = data as GuarantorTableElement[];
+          this.guarantorsList = tmp;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
   }
 
 }
