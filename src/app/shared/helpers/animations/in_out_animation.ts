19b357fb7ce4993ee123de5c4e8daef5fdd6ad8c
Andrejs Naumovs <andrejs@naumovs.de>
2020-09-20 13:45:19 +0300

    minory chages - formatter

--- src/app/shared/helpers/animations/in_out_animation.ts
+++ src/app/shared/helpers/animations/in_out_animation.ts
@@ -3,23 +3,14 @@ import { trigger, transition, style, animate } from '@angular/animations';
 // cut & copy from: https://medium.com/ngconf/animating-angulars-ngif-and-ngfor-32a6ff26ed2d
 
 export function inOutAnimation() {
-  return trigger(
-    'inOutAnimation',
-    [
-      transition(
-        ':enter',
-        [
-          style({ height: 0, opacity: 0 }),
-          animate('250ms ease-out', style({ height: 300, opacity: 1 }))
-        ]
-      ),
-      transition(
-        ':leave',
-        [
-          style({ height: 300, opacity: 1 }),
-          animate('250ms ease-in', style({ height: 0, opacity: 0 }))
-        ]
-      )
-    ]
-  )
+  return trigger('inOutAnimation', [
+    transition(':enter', [
+      style({ height: 0, opacity: 0 }),
+      animate('250ms ease-out', style({ height: 300, opacity: 1 })),
+    ]),
+    transition(':leave', [
+      style({ height: 300, opacity: 1 }),
+      animate('250ms ease-in', style({ height: 0, opacity: 0 })),
+    ]),
+  ]);
 }
