6fd45fde93becb33a9aca0a8207a57a0233dc827
Andrejs Naumovs <andrejs@naumovs.de>
2020-10-17 17:36:01 +0300

    refactoring & added: title service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { Router, NavigationEnd } from '@angular/router';
+
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
@@ -8,18 +10,26 @@ import { TranslateService } from '@ngx-translate/core';
   template: '<ng-http-loader></ng-http-loader><router-outlet></router-outlet><ng-snotify></ng-snotify>'
 })
 export class AppComponent implements OnInit {
-  constructor(private router: Router,
-    public translate: TranslateService) {
-    translate.addLangs(['en', 'ru', 'lv', 'de']);
-    // translate.setDefaultLang('en');
-    translate.setDefaultLang('ru');
-
-    const browserLang = translate.getBrowserLang();
-    // translate.use(browserLang.match(/ru|lv/) ? browserLang : 'en');
-    translate.use('ru');
-  }
+  constructor(
+    private titleService: Title,
+    private router: Router,
+    public translate: TranslateService
+  ) { }
 
   ngOnInit() {
+    this.translate.addLangs(['en', 'ru', 'lv', 'de']);
+    // TODO: set default 'en' after debuging
+    this.translate.setDefaultLang('ru');
+
+    const browserLang = this.translate.getBrowserLang();
+    this.translate.use('ru');
+    // TODO: set use from browser again - after debuging
+    // translate.use(browserLang.match(/ru|lv/) ? browserLang : 'en');
+
+    this.translate.get('title').subscribe((title: string) => {
+      this.titleService.setTitle(title);
+    });
+
     this.router.events.subscribe((evt) => {
       if (!(evt instanceof NavigationEnd)) {
         return;
